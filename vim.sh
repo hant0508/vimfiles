ecbafbbd4eb3a454f1657cf4b15ed9c15e36c9de
hant05080 <hant0508@gmail.com>
2017-09-22 17:58:00 +0300

    Add script and fonts

---
+++ vim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Configure vim? (y/n): " -r
+if [[ $REPLY =~ ^[NnНн]$ ]]
+then
+	exit
+fi
+
+echo "Copying files..."
+cp -r .vim ~/
+cp -r .vimrc ~/
+cp -r .fonts ~/
+echo "Installing fonts..."
+fc-cache -f
+echo "Done!"
+vim 1.cpp
+read -p "Remove this folder? (y/n): " -r
+if [[ $REPLY =~ ^[YyДд]$ ]]
+then
+	rm -rf `pwd`
+fi
